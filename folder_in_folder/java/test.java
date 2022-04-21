4548125e26a5c7e19ff80cb257ee2e6695c6065f
Corey Wang <coreyw21042@gmail.com>
2022-04-20 22:37:37 -0400

    more stufff

--- folder_in_folder/java/test.java
+++ folder_in_folder/java/test.java
@@ -1,5 +1,9 @@
 class test{
     public static void main(String[] args){
         System.out.println("hello");
+        int a=3;
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
     }
 }
