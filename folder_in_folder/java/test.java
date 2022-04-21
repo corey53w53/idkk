8b15db2c94e964039d5026ad3d43980db981df58
Corey Wang <coreyw21042@gmail.com>
2022-04-20 22:39:12 -0400

    function

--- folder_in_folder/java/test.java
+++ folder_in_folder/java/test.java
@@ -1,9 +1,15 @@
 class test{
     public static void main(String[] args){
         System.out.println("hello");
-        int a=3;
         for (int i=0;i<10;i++){
             System.out.println(i);
         }
+        int w = 2;
+        int e = plusone(w);
+        System.out.println(e);
+    }
+
+    public static int plusone(int a){
+        return a+1;
     }
 }
