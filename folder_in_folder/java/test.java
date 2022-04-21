a193ae563e7cdf4ba1576af46e1027cef4d71770
Corey Wang <coreyw21042@gmail.com>
2022-04-20 22:41:43 -0400

    point

--- folder_in_folder/java/test.java
+++ folder_in_folder/java/test.java
@@ -7,9 +7,15 @@ class test{
         int w = 2;
         int e = plusone(w);
         System.out.println(e);
+        Point(3,4);
     }
 
     public static int plusone(int a){
         return a+1;
+    }
+    public static void Point(int a, int b){
+        int x = a;
+        int y = b;
+        System.out.println("("+x+","+y+")");
     }
 }
